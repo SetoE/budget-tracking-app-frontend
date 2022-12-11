40f1419542ef819feac68499835f3c4ec7d00324
SetoE <sethjespinas@gmail.com>
2022-12-11 16:57:35 +0800

    Setup login feature

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -11,18 +11,18 @@
         free</router-link>
     </p>
   </div>
-  <form class="mt-8 space-y-6" action="#" method="POST">
+  <form class="mt-8 space-y-6" @submit="login">
     <input type="hidden" name="remember" value="true" />
     <div class="-space-y-px rounded-md shadow-sm">
       <div>
         <label for="email-address" class="sr-only">Email address</label>
-        <input id="email-address" name="email" type="email" autocomplete="email" required=""
+        <input id="email-address" name="email" type="email" autocomplete="email" required="" v-model="user.email"
           class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
           placeholder="Email address" />
       </div>
       <div>
         <label for="password" class="sr-only">Password</label>
-        <input id="password" name="password" type="password" autocomplete="current-password" required=""
+        <input id="password" name="password" type="password" autocomplete="current-password" required="" v-model="user.password"
           class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
           placeholder="Password" />
       </div>
@@ -30,7 +30,7 @@
 
     <div class="flex items-center justify-between">
       <div class="flex items-center">
-        <input id="remember-me" name="remember-me" type="checkbox"
+        <input id="remember-me" name="remember-me" type="checkbox" v-model="user.remember"
           class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
         <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
       </div>
@@ -48,10 +48,27 @@
   </form>
 </template>
   
-<script>
+<script setup>
 import { LockClosedIcon } from '@heroicons/vue/20/solid'
+import store from '../store'
+import { useRouter } from 'vue-router'
 
-export default {
-  name: 'Login',
+const router = useRouter();
+
+const user = {
+  email: '',
+  password: '',
+  remember: false,
+}
+
+function login(ev) {
+  ev.preventDefault();
+
+  store.dispatch('login', user)
+    .then(() => {
+      router.push({
+        name: 'Dashboard',
+      })
+    });
 }
 </script>
