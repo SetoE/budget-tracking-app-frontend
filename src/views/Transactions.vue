408424fd9350a4d01d7e0aeac2eac3f72ff1481f
SetoE <sethjespinas@gmail.com>
2022-12-11 12:08:30 +0800

    Implemented PageComponent for common htmls in default layout

--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -1,17 +1,9 @@
 <template>
-  <header class="bg-white shadow">
-    <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">All Transactions</h1>
-    </div>
-  </header>
-  <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-      <!-- Replace with your content -->
-      <div class="px-4 py-6 sm:px-0">
-        Content Goes Here
-      </div>
-      <!-- /End replace -->
-    </div>
-  </main>
+  <PageComponent title="Transactions">
+    content goes here
+  </PageComponent>
 </template>
 
+<script setup>
+import PageComponent from '../components/PageComponent.vue';
+</script>
