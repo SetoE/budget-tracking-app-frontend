9fcc0d691389f5ecf9753b869e20b6ff091038ac
SetoE <sethjespinas@gmail.com>
2022-12-11 10:47:51 +0800

    Moved login and register to auth group for grouping

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
 
 import DefaultLayout from '../components/DefaultLayout.vue'
+import AuthLayout from '../components/AuthLayout.vue'
 import Dashboard from '../views/Dashboard.vue'
 import Transactions from '../views/Transactions.vue'
 import Login from '../views/Login.vue'
@@ -11,26 +13,45 @@ const routes = [
     path: '/',
     redirect: '/dashboard',
     component: DefaultLayout,
+    meta: { requiresAuth: true },
     children: [
       { path: '/dashboard', name: 'Dashboard', component: Dashboard },
       { path: '/transactions', name: 'Transactions', component: Transactions },
     ],
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login,
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: Register,
+    path: '/auth',
+    redirect: '/login',
+    name: 'Auth',
+    component: AuthLayout,
+    meta: { isGuest: true },
+    children: [
+      {
+        path: '/login',
+        name: 'Login',
+        component: Login,
+      },
+      {
+        path: '/register',
+        name: 'Register',
+        component: Register,
+      },
+    ],
   },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    next({ name: 'Login' });
+  } else if (store.state.user.token && to.meta.isGuest) {
+    next({ name: 'Dashboard' });
+  }
+  next();
 });
 
 export default router;
