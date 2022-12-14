db89cd3583fb9b873bf9d2b11525315b37b3c3e8
Setonator <sethjespinas@gmail.com>
2022-12-14 17:43:14 +0800

    Initial transfer setup

---
+++ src/views/Transfers.vue
@@ -0,0 +1,14 @@
+<template>
+    <PageComponent>
+        <template v-slot:header>
+            <div class="flex justify-between items-center">
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Transfers</h1>
+                <router-link :to="{ name: 'TransactionCreate' }"
+                    class="py-2 px-3 text-white bg-emerald-500 rounded-md hover:bg-emerald-600 flex justify-between items-center">
+                    <PlusIcon class="h-5 w-5 text-white"></PlusIcon>
+                    Add new Transfers
+                </router-link>
+            </div>
+        </template>
+    </PageComponent>
+</template>
