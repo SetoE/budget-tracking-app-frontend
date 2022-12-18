826814588eced434214badab4534b4266707cb1d
SetoE <sethjespinas@gmail.com>
2022-12-19 05:18:49 +0800

    Initial setup for transfer editor view

---
+++ src/views/editors/TransferEditor.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="relative rounded-md p-3 hover:bg-gray-100">
+    <h3 class="text-sm font-medium leading-5">{{model.name}}</h3>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+  transfer: Object,
+  index: Number,
+});
+
+const emit = defineEmits(["change", "addTransfer", "deleteTransfer"]);
+
+const model = ref(JSON.parse(JSON.stringify(props.transfer)));
+</script>
